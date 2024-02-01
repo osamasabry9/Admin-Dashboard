90e5a1805fcf3c6c5788d620a45c81c2eb014ff9
osamasabry9 <osamasabry324@gmail.com>
2024-02-01 14:14:03 +0200

    Create custom button

---
+++ lib/presentation/widgets/custom_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../core/utils/app_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      this.title = 'Send Money',
+      this.backgroundColor,
+      this.textColor});
+
+  final Color? backgroundColor, textColor;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          elevation: 0,
+          backgroundColor: backgroundColor ?? const Color(0xFF4DB7F2),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: AppStyles.styleSemiBold18(context).copyWith(
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
