8ca3cf52f082b4d42f9c6762c17eacbffb69a1cf
osamasabry9 <osamasabry324@gmail.com>
2024-02-04 16:00:31 +0200

    add All Expenses And Quick Invoice Section in file

---
+++ lib/presentation/widgets/all_expenses_and_quick_invoice_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/expenses_widgets/all_expenses.dart';
+import '../widgets/quick_invoice_widgets/quick_invoice.dart';
+
+class AllExpensesAndQuickInvoiceSection extends StatelessWidget {
+  const AllExpensesAndQuickInvoiceSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          AllExpenses(),
+          SizedBox(
+            height: 24,
+          ),
+          QuickInvoice(),
+        ],
+      ),
+    );
+  }
+}
