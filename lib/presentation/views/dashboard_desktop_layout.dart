3f52794762c39899ffd69de90c8cd9e134256996
osamasabry9 <osamasabry324@gmail.com>
2024-02-05 16:06:25 +0200

    Finish desktop layout

--- lib/presentation/views/dashboard_desktop_layout.dart
+++ lib/presentation/views/dashboard_desktop_layout.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../widgets/all_expenses_and_quick_invoice_section.dart';
 
 import '../widgets/drawer_widgets/custom_drawer.dart';
+import '../widgets/income_widgets/income_section.dart';
 import '../widgets/my_cards_and_transaction_history_section.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
@@ -10,19 +11,18 @@ class DashBoardDesktopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 40),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: CustomDrawer(),
-          ),
-          SizedBox(
-            width: 32,
-          ),
-          Expanded(
-            flex: 7,
+    return const Row(
+      children: [
+        Expanded(
+          child: CustomDrawer(),
+        ),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: EdgeInsets.only(top: 40),
             child: CustomScrollView(
               slivers: [
                 SliverFillRemaining(
@@ -30,19 +30,22 @@ class DashBoardDesktopLayout extends StatelessWidget {
                   child: Row(
                     children: [
                       Expanded(
-                        flex: 4,
+                        flex: 2,
                         child: AllExpensesAndQuickInvoiceSection(),
                       ),
                       SizedBox(
                         width: 24,
                       ),
                       Expanded(
-                        flex: 3,
+                        flex: 1,
                         child: Column(
                           children: [
                             MyCardsAndTransactionHistorySection(),
                             SizedBox(
                               height: 24,
+                            ),
+                            Expanded(
+                              child: IncomeSection(),
                             ),
                           ],
                         ),
@@ -52,9 +55,9 @@ class DashBoardDesktopLayout extends StatelessWidget {
                 )
               ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
